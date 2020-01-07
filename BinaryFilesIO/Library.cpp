92c24e47dcf791a5b0ff5a59233724ae759d37d9
Jesse Simurdak <jessesimurdak@gmail.com>
2020-01-07 12:58:15 -0800

    binary file

--- BinaryFilesIO/Library.cpp
+++ BinaryFilesIO/Library.cpp
@@ -8,34 +8,73 @@ Library::Library()
 
 void Library::loadRecords()
 {
-	file.open("library.dat", std::ios::in | std::ios::binary);
+	file.open("library.dat", ios::in | ios::binary);
 	if (file.is_open())
 	{
-		file.read((char*)&buffer, sizeof(Library));
+		file.read((char*)records, sizeof(*records));
 		file.close();
 	}
 	else
 	{
-		file.open("library.txt", std::ios::out);
+		file.open("library.txt", ios::out);
+		file.close();
 		if (!file.is_open())
 		{
 			std::cerr << "'library' file error" << endl;
 		}
 		else
 		{
+			file.open("library.txt", ios::in);
+			for (int i = 0; i < entries; ++i)
+			{
+				getline(file, buffer);		libData[0] = buffer;
+				getline(file, buffer);		libData[1] = buffer;
+				getline(file, buffer);		libData[2] = buffer;
+				getline(file, buffer);		libData[3] = buffer;
+				getline(file, buffer);		libData[4] = buffer;
+				getline(file, buffer);		libData[5] = buffer;
+				file.flush();
+				file.clear();
+			}
+			file.close();
 
+			file.open("library.dat", ios::out | ios::binary);
+			file.write((char*)records, sizeof(*records));
+			file.close();
 		}
 	}
 }
 
 void Library::saveRecords()
 {
-
+	file.open("library.dat", ios::out | ios::binary);
+	file.write((char*)records, sizeof(*records));
+	file.close();
 }
 
 void Library::findRecord()
 {
-
+	bool fail = true;
+	char selection[9];
+	cout << "Enter call number of book:" << endl;
+	cin >> selection;
+	file.open("library.txt", ios::in);
+	file.read((char*)records, sizeof(*records));
+	while (fail)
+	{
+		if (selection == records->callNum)
+		{
+			system("pause");
+			file.close();
+			fail = false;
+		}
+		else
+		{
+			cout << "Entry not found, please try again:" << endl;
+			cin >> selection;
+			fail = true;
+		}
+	}
 }
 
 void Library::displayRecord()
--- BinaryFilesIO/Library.h
+++ BinaryFilesIO/Library.h
@@ -11,6 +11,8 @@ using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::ios;
+
 
 class Library
 {
@@ -24,15 +26,21 @@ public:
 	void addRecord();
 	~Library();
 
+	struct LibraryData
+	{
+		char** libData = new char*[6];
+		char callNum[9] = libData[0];
+		char title[20] = libData[1];
+		char author[20] = libData[2];
+		char bookStatus[1] = libData[3];
+		char dueDate[10] = libData[4];
+		char borrower[20] = libData[5];
+	};
+
 protected:
 	std::fstream file;
 	string buffer;
-	string callNum;
-	string title;
-	string author;
-
-	int bookStatus = 0;
-	string dueDate;
-	string borrower;
+	int entries = 0;
+	LibraryData* records = new LibraryData[entries];
 };
 
